bebf6bcf820b723f59096df886edb44574a2ac21
pyguru123 <pyguru123@gmail.com>
2023-08-01 00:13:52 +0530

    added iss locator

--- app/openspace/main.py
+++ app/openspace/main.py
@@ -3,7 +3,7 @@ import pytz
 import platform
 import requests
 from loguru import logger
-from datetime import datetime
+from datetime import datetime, timedelta
 
 if platform.system() == "Windows":
     from dotenv import load_dotenv
@@ -13,11 +13,23 @@ if platform.system() == "Windows":
 api_token = os.environ.get("NASA_TOKEN")
 indian_tz = pytz.timezone('Asia/Kolkata')
 
-def todays_date():
-    today = datetime.now(indian_tz)
+def todays_date(delta=0):
+    today = datetime.now(indian_tz) - timedelta(days=delta)
     return today.strftime("%Y-%m-%d")
 
 async def astronomy_pic_of_day(date):
 	url = f"https://api.nasa.gov/planetary/apod?date={date}&api_key={api_token}"
+	response = requests.get(url)
+	if response.status_code == 200:
+		return response.json()
+
+	data = response.json()
+	if "Date must be between" in data["msg"]:
+		url = f"https://api.nasa.gov/planetary/apod?date={todays_date(1)}&api_key={api_token}"
+		response = requests.get(url)
+		return response.json()
+
+async def where_is_iss():
+	url = "https://api.wheretheiss.at/v1/satellites/25544"
 	response = requests.get(url)
 	return response.json()
--- app/openspace/router.py
+++ app/openspace/router.py
@@ -4,13 +4,15 @@ from typing import Union
 
 from app.openspace.main import (
     todays_date,
-    astronomy_pic_of_day
+    astronomy_pic_of_day,
+    where_is_iss
 )
 
 router = APIRouter()
 
 @router.get("/apod/today")
 async def apod_today():
+    """NASA Astronomy picture of the day"""
     try:
         date = todays_date()
         response = await astronomy_pic_of_day(date)
@@ -23,8 +25,22 @@ async def apod_today():
 
 @router.get("/apod/date")
 async def apod(date):
+    """Date Format : yyyy-mm-dd"""
     try:
         response = await astronomy_pic_of_day(date)
+        return response
+
+    except Exception as e:
+        return {
+            "message":"error",
+            "error":str(e)
+        }
+
+@router.get("/iss")
+async def iss_info():
+    """Get the current position, velocity and altitude of ISS"""
+    try:
+        response = await where_is_iss()
         return response
 
     except Exception as e:
