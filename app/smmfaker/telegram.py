1c1a5aa4529ae9e92bf5eae1a9491b95fac2054b
pyguru123 <pyguru123@gmail.com>
2023-08-15 01:08:54 +0530

    updated tg views

--- app/smmfaker/telegram.py
+++ app/smmfaker/telegram.py
@@ -2,10 +2,23 @@ import random
 import requests
 from loguru import logger
 
-async def get_tg_views(post_url: str, views: int):
-    order_id = random.randint(1000, 9999)
-    endpoint = f"https://smmblak.com/order/38{order_id}"
+async def make_request(order_id, payload, MAX_RETRIES):
+    if MAX_RETRIES > 0:
+        endpoint = f"https://smmblak.com/order/380{order_id:03}"
+        logger.info(endpoint)
 
+        response = requests.request("POST", endpoint, data=payload)
+        if response.text.strip().startswith("<!DOCTYPE html>"):
+            return "success"
+            
+        return response.text.strip()
+
+    return "failed"
+
+async def get_tg_views(post_url: str, views: int):
+    MAX_RETRIES = 10
+
+    order_id = random.randint(50, 100)
     payload = {
         'categories': ' 315',
         'services': ' 3060',
@@ -13,8 +26,8 @@ async def get_tg_views(post_url: str, views: int):
         'quantity': views
     }
 
-    response = requests.request("POST", endpoint, data=payload)
-    if response.text.startswith("<!DOCTYPE html>"):
-        return "success"
-        
-    return "failed" 
+    response = await make_request(order_id, payload, MAX_RETRIES)
+    if response != "success":
+        make_request(order_id+1, payload, MAX_RETRIES-1)
+
+    return "success"
