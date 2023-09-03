266eaa0065352225e43e883060e5881d1a71abbe
pyguru123 <pyguru123@gmail.com>
2023-09-03 23:24:11 +0530

    added ytsmx leecher

--- app/leechers/router.py
+++ app/leechers/router.py
@@ -3,7 +3,8 @@ from typing import Union
 from fastapi.responses import Response
 from loguru import logger
 
-from app.leechers.piratesbay import get_magnets
+from app.leechers.piratesbay import get_piratesbay_magnets
+from app.leechers.ytsmx import get_yts_magnet
 
 
 router = APIRouter()
@@ -13,7 +14,16 @@ router = APIRouter()
 async def piratesbay(movie: str) -> list[dict]:
     """Get Magnet Links directly from piratesbay"""
     try:
-        result = await get_magnets(movie)
+        result = await get_piratesbay_magnets(movie)
         return result
     except Exception as e:
-        raise HTTPException(status_code=404, details="Unable to fetch magnets")
+        raise HTTPException(status_code=404, detail="Unable to fetch magnets")
+
+@router.get("/ytsmx/{movie}")
+async def ytsmx(movie: str) -> list[dict]:
+    """Get Magnet Links directly from piratesbay"""
+    try:
+        result = await get_yts_magnet(movie)
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=404, detail="Unable to fetch magnets")
--- app/leechers/ytsmx.py
+++ app/leechers/ytsmx.py
@@ -1,30 +1,28 @@
 import requests
 from loguru import logger
 
-async def bytes_to_gb(bytes):
-    gb = bytes / 1_073_741_824
-    return f"{gb:.2f}gb"
-
 async def create_magnet(name, hash):
 	name = name.replace(" ", "%20")
 	magnet = f"magnet:?xt=urn:btih:{hash}&dn={name}&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.bittor.pw%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fbt.xxx-tracker.com%3A2710%2Fannounce&tr=udp%3A%2F%2Fpublic.popcorn-tracker.org%3A6969%2Fannounce&tr=udp%3A%2F%2Feddie4.nl%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce"
 	return magnet
 
-async def get_piratesbay_magnets(movie):
+async def get_yts_magnet(movie):
 	movie_name = movie.replace(" ", "%20")
-	url = f"https://apibay.org/q.php?q={movie_name}&cat=0"
+	url = f"https://yts.mx/api/v2/list_movies.json?query_term={movie_name}"
 	response = requests.get(url)
 	all_data = response.json()
 	result_set = []
 
-	if all_data[0]['name'] != "No results returned":
-		for data in all_data:
-			result = {
-				"name" : data['name'], 
-				"size" : await bytes_to_gb(int(data['size'])),
-				"magnet" : await create_magnet(data['name'], data['info_hash'])
-			}
-			result_set.append(result)
+	if all_data['status'] == 'ok':
+		for data in all_data['data']['movies']:
+			logger.info(data['torrents'])
+			for torrent in data["torrents"][:3]:
+				result = {
+					"name": f"{data['title_long']} {torrent['quality']}",
+					"size": torrent["size"],
+					"magnet": await create_magnet(torrent['hash'], data["title_long"])
+				}
+				result_set.append(result)
 
 		return result_set
 
