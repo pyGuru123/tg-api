41a7e2e34cca592d5dceddf636908904fcf3a249
Prajjwal Pathak <prajjwal.pathak@kapso.in>
2024-01-14 13:31:45 +0530

    removed unused endpoints

--- app/llmodels/llm.py
+++ app/llmodels/llm.py
@@ -7,51 +7,22 @@ from loguru import logger
 
 if platform.system() == "Windows":
     from dotenv import load_dotenv
-
     load_dotenv()
 
-CHIMERA_ENDPOINT = os.environ.get("CHIMERA_ENDPOINT")
-CHIMERA_TOKEN = os.environ.get("CHIMERA_TOKEN")
-BAI_ORG_ID = os.environ.get("BAI_ORG_ID")
-BAI_TOKEN = os.environ.get("BAI_TOKEN")
-PALM_TOKEN = os.environ.get("PALM_TOKEN")
-GPT_ENDPOINT = os.environ.get("GPT_ENDPOINT")
-GPT4_ENDPOINT = os.environ.get("GPT4_ENDPOINT")
+GPT4_ENDPOINT = "https://chat.gpt.bz/api/openai/v1/chat/completions?conversation_id=qao5oNacJChB9DRNbbTT-"
+GEMINI_TOKEN = os.environ.get("GEMINI_TOKEN")
 GPT4_TOKEN = os.environ.get("GPT4_TOKEN")
-
-async def ask_bai(prompt: str):
-    req_rand = random.randint(100000, 999999)
-    url = f"https://beta.theb.ai/api/conversation?org_id={BAI_ORG_ID}&req_rand=0.397137{req_rand}"
-
-    payload = json.dumps({
-        "text": prompt,
-        "category": "5be3c43f8bc740b792cce30cebdd861c",
-        "model": "12cf0aaece3f4c27846aeb9c852dc0f9",
-        "model_params": {},
-        "topic_id": None,
-        "stream": False
-    })
-
-    headers = {
-        'Authorization': f'Bearer {BAI_TOKEN}',
-        'Content-Type': 'application/json',
-    }
-
-    response = requests.request("POST", url, headers=headers, data=payload)
-    data = response.text
-    data = data.split("event: ")
-    response = data[-2].strip().replace('{"content": "', "").split('"}')[0].replace('update\r\ndata: ', "")
-    return response.strip().strip('"')
-
 
 async def ask_llama(prompt: str):
     payload = json.dumps({
-      "prompt": prompt,
-      "version": "2796ee9483c3fd7aa2e171d38f4ca12251a30609463dcfd4cd76703f22e96cdf",
-      "systemPrompt": "You are a helpful assistant.",
-      "temperature": 0.75,
-      "topP": 0.9,
-      "maxTokens": 800
+        "prompt": f"<s>[INST] <<SYS>>\nYou are a helpful assistant.\n<</SYS>>\n\n{prompt}[/INST]\n",
+        "model": "meta/llama-2-70b-chat",
+        "systemPrompt": "You are a helpful assistant.",
+        "temperature": 0.75,
+        "topP": 0.9,
+        "maxTokens": 800,
+        "image": None,
+        "audio": None
     })
 
     headers = {
@@ -62,63 +33,32 @@ async def ask_llama(prompt: str):
     return response.text
 
 
-async def ask_palm(prompt: str):
+async def ask_gemini(prompt: str):
     headers = {
-        'Content-Type': 'application/json',
-    }
-
-    params = {
-        'key': PALM_TOKEN,
-    }
-
-    json_data = {
-        'prompt': {
-            "messages": [{"content":prompt}]
-        },
-    }
-
-    response = requests.post(
-        'https://generativelanguage.googleapis.com/v1beta2/models/chat-bison-001:generateMessage',
-        params=params,
-        headers=headers,
-        json=json_data,
-    )
-
-    return response.json()["candidates"][0]["content"]
-
-
-async def ask_gpt(prompt: str, context: str):
-    if not context:
-        context ="You are ChatGPT, a large language model trained by OpenAI.\
-                Carefully heed the user's instructions.\
-                Respond using Markdown."
-
-    payload = json.dumps({
-      "messages": [
-        {
-          "role": "system",
-          "content": context
-        },
-        {
-          "role": "user",
-          "content": prompt
-        }
-      ],
-      "model": "gpt-3.5-turbo",
-      "temperature": 1,
-      "presence_penalty": 0,
-      "top_p": 1,
-      "frequency_penalty": 0,
-      "stream": False
-    })
-
-    headers = {
-      'Authorization': 'Bearer pk-this-is-a-real-free-pool-token-for-everyone',
       'Content-Type': 'application/json'
     }
 
-    response = requests.request("POST", GPT_ENDPOINT, headers=headers, data=payload)
-    return response.json()["choices"][0]["message"]["content"]
+    payload = json.dumps({
+      "contents": [
+        {
+          "parts": [
+            {
+              "text": prompt
+            }
+          ]
+        }
+      ]
+    })
+
+    response = requests.post(
+        f'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key={GEMINI_TOKEN}',
+        headers=headers,
+        data=payload,
+    )
+
+    logger.info(response.json())
+    return response.json()["candidates"][0]["content"]["parts"][0]["text"]
+
 
 async def ask_gpt4(prompt: str, context: str):
     if not context:
--- app/llmodels/router.py
+++ app/llmodels/router.py
@@ -4,44 +4,17 @@ from fastapi.responses import Response
 from loguru import logger
 
 from app.model import llmRequest, llmResponse
-from app.llmodels.bard import ask_bard
 from app.llmodels.llm import (
-    ask_gpt,
     ask_gpt4,
     ask_llama,
-    ask_bai,
-    ask_palm
+    ask_gemini
 )
 
 router = APIRouter()
 
-
 @router.post("/gpt")
-async def gpt(request: llmRequest) -> llmResponse:
-    """Search ChatGPT using gpt-3.5-turbo model"""
-    try:
-        prompt = request.prompt
-        context = request.context
-        data = await ask_gpt(prompt, context)
-        logger.info(f"{data=}")
-
-        return llmResponse(
-            message="success",
-            prompt=prompt,
-            content=data,
-            error=""
-        )
-    except Exception as e:
-        return llmResponse(
-            message="error",
-            prompt=prompt,
-            content="",
-            error=str(e)
-        )
-
-@router.post("/gpt4")
 async def gpt4(request: llmRequest) -> llmResponse:
-    """Search ChatGPT using gpt-3.5-turbo model"""
+    """Search ChatGPT using gpt-4 model"""
     try:
         prompt = request.prompt
         context = request.context
@@ -84,12 +57,12 @@ async def llama(request: llmRequest) -> llmResponse:
         )
 
 
-@router.post("/bai")
-async def bai(request: llmRequest) -> llmResponse:
-    """Search chatgpt using b.ai interface"""
+@router.post("/gemini")
+async def gemini(request: llmRequest) -> llmResponse:
+    """Search Google advanced AI Gemini"""
     try:
         prompt = request.prompt
-        data = await ask_bai(prompt)
+        data = await ask_gemini(prompt)
 
         return llmResponse(
             message="success",
@@ -104,47 +77,3 @@ async def bai(request: llmRequest) -> llmResponse:
             content="",
             error=str(e)
         )
-
-
-@router.post("/palm")
-async def palm(request: llmRequest) -> llmResponse:
-    """Search chatgpt using b.ai interface"""
-    try:
-        prompt = request.prompt
-        data = await ask_palm(prompt)
-
-        return llmResponse(
-            message="success",
-            prompt=prompt,
-            content=data,
-            error=""
-        )
-    except Exception as e:
-        return llmResponse(
-            message="error",
-            prompt=prompt,
-            content="",
-            error=str(e)
-        )
-
-
-# @router.post("/bard")
-# async def bard(request: llmRequest) -> llmResponse:
-#     """Search Google Bard in a sessioned state"""
-#     try:
-#         prompt = request.prompt
-#         data = await ask_bard(prompt)
-
-#         return llmResponse(
-#             message="success",
-#             prompt=prompt,
-#             content=data,
-#             error=""
-#         )
-#     except Exception as e:
-#         return llmResponse(
-#             message="error",
-#             prompt=prompt,
-#             content="",
-#             error=str(e)
-#         )
